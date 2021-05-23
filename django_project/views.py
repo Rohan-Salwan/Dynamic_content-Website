5b12accb08a52b78c1448295c2c06323e629eb75
Rohan Salwan <rohansalwan23@gmail.com>
2021-05-24 04:19:08 +0530

    new update

--- django_project/views.py
+++ django_project/views.py
@@ -4,7 +4,15 @@ from .models import exp
 
 def index(request):
     u1=exp()
-    u1.name='pizaaaaa'
+    u1.name='virgin sala'
     u1.price='7000'
     u1.des='awesome pizza'
     return render(request, 'index.html',{'u1':u1})
+
+def login(request):
+    na=request.GET['fname']
+    naa=request.GET['lname']
+    re=na+naa
+    return render(request, 'result.html', {"result":re})
+
+    
